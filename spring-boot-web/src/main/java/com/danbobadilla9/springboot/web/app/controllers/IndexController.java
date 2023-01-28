551e049aac5171aae61be2ab2e7cf8cd4b9ec528
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-01-27 23:08:53 -0600

    Nada Relevante

--- spring-boot-web/src/main/java/com/danbobadilla9/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/danbobadilla9/springboot/web/app/controllers/IndexController.java
@@ -1,15 +1,21 @@
 package com.danbobadilla9.springboot.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping("/index")
-	public String index() {
+	@GetMapping({"/index","/","/home"})
+	public String index(Model model ) {
+		model.addAttribute("titulo","Hola enviando datos con Model");
 		return "index";
 	}
 	
