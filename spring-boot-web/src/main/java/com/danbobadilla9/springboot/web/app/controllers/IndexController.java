31e627590a01ac82346c2882a57e679a1ad2d294
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-01-29 13:52:06 -0600

    Agregando Nuevos thymeleaf

--- spring-boot-web/src/main/java/com/danbobadilla9/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/danbobadilla9/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.danbobadilla9.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.danbobadilla9.springboot.web.app.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -18,5 +22,68 @@ public class IndexController {
 		model.addAttribute("titulo","Hola enviando datos con Model");
 		return "index";
 	}
+	
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Dan");
+		usuario.setApellido("Bobadilla");
+		usuario.setEmail("danbobadilla9@hotmail.com");
+		model.addAttribute("usuario",usuario);
+		model.addAttribute("title","Perfil del Usuario: ".concat(usuario.getNombre()));
+		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario("Dan","Bobadilla","danobadilla9@hotmail.com"));
+		usuarios.add(new Usuario("Israel","Segundo","danisrael@hotmail.com"));
+		usuarios.add(new Usuario("Jose","Perez","josePerez9@hotmail.com"));
+		model.addAttribute("title","Listado de usuarios");
+		model.addAttribute("usuarios",usuarios);
+		return "listar";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
