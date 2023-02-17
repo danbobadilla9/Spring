b6b86f2af03edf473ef4467cfcc6697fe847283b
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-02-17 11:22:41 -0600

    inyeccion de dependencias

---
+++ spring-boot-di/src/main/java/com/danbobadilla9/springboot/di/models/service/MiServicio.java
@@ -0,0 +1,46 @@
+package com.danbobadilla9.springboot.di.models.service;
+
+
+
+
+public class MiServicio {
+	
+
+	public String operacion() {
+		
+		return "Ejecutando algun proceso importante...";
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ spring-boot-di/src/test/java/com/danbobadilla9/springboot/di/app/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danbobadilla9.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
